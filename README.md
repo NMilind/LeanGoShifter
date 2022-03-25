ea934d77d0d9bc86971890b96f895ecf3392fdfb
Nikhil Milind <nikhilmilind@gmail.com>
2022-03-25 10:05:05 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,101 @@
+# Lean GoShifter
+
+[GoShifter](https://github.com/immunogenomics/goshifter) is an analysis tool to test for enrichment of GWAS loci in genomic annotations. For details, please refer to the paper.
+
+Trynka G, Westra H-J, Slowikowski K, Hu X, Xu H, Stranger BE, et al. Disentangling the Effects of Colocalizing Genomic Annotations to Functionally Prioritize Non-coding Variants within Complex-Trait Loci. The American Journal of Human Genetics. 2015 Jul 2;97(1):139–52. 
+[https://doi.org/10.1016/j.ajhg.2015.05.016](https://doi.org/10.1016/j.ajhg.2015.05.016).
+
+I have reimplemented the tool in Python 3 with a reduced feature set:
+
+1. Rather than calculating SNPs in LD with the lead SNP at a locus, Lean GoShifter expect the user to have pre-calculated a fine-mapped SNP set for each locus.
+2. Lean GoShifter does not implement the conditional analysis for colocalizing annotations.
+
+## Requirements
+
+Lean GoShifter is written for **Python 3.8**. It uses the following base modules:
+
+1. `argparse`
+2. `math`
+3. `os`
+4. `sys`
+
+In addition, Lean GoShifter requires the following modules:
+
+1. `numpy >= 1.21.3`
+2. `pandas >= 1.3.4`
+
+You can install these using the following command:
+
+```
+python3 -m pip install numpy==1.21.3 pandas==1.3.4
+```
+
+## How to use Lean GoShifter
+
+### Description
+
+The script takes as input a list of SNPs assigned to each locus. These may be the lead SNP and tagging LD SNPs or a fine-mapped set of SNPs around the lead SNP. The script also takes a set of genomic intervals that represent annotations.
+
+Loci are calculated as the furthest SNPs at each locus extended by twice the median size of the annotation. The annotations at each locus are permuted and the enrichment of overlapping SNPs at loci is estimated using the null distribution. Refer to the original paper for specific details.
+
+### Usage
+
+```
+usage: goshifter.py [-h] snp_map annotation permute out_dir prefix
+
+A lean reimplementation of GoShifter
+
+positional arguments:
+  snp_map     A table of SNPs for each individual signal.
+  annotation  File with genomic regions representing annotations.
+  permute     Number of permutations for the null distribution.
+  out_dir     Output directory.
+  prefix      Prefix for output files.
+
+optional arguments:
+  -h, --help  show this help message and exit
+```
+
+### Inputs
+
+This is the expected format for the `snp_map` argument. The file is tab-delimited. The `Locus` column indicates separate signals to test for enrichment in the genomic annotations. This example contains 2 loci with a fine-mapped set of one SNP and a third locus with a fine-mapped set of 7 SNPs.
+
+```
+Locus	SNP	Chr	Position
+1	rs3131972	1	817341
+2	rs2272757	1	946247
+3	rs13302945	1	953779
+3	rs13302957	1	955641
+3	rs13302996	1	966227
+3	rs13303051	1	954333
+3	rs13303056	1	953778
+3	rs13303206	1	954258
+3	rs13303227	1	957365
+```
+
+This is the expected format for the `annotation` argument. The file is tab-delimited.
+
+```
+Chr	Start	End
+1	23224	23849
+1	24025	26778
+1	28524	30136
+1	30354	30875
+1	31109	31894
+1	32151	32330
+1	32460	33367
+1	33645	33887
+1	33988	35232
+```
+
+### Example
+
+```
+mkdir test_output/
+python3 lean_go_shifter.py snp_map.tsv annotation.tsv 1000 test_output/ prefix
+```
+
+### Outputs
+
+1. `test_output/prefix_p_value.tsv` - Contains the p-value of enrichment of loci overlapping the genomic annotation.
+2. `test_output/prefix_overlap_scores.tsv` - Contains the loci that were tested for enrichment. A column indicates if the locus overlapped any genomic annotations. Another column indicates the likelihood of an overlap occuring, with low scores representing unlikely overlap that drives the enrichment of the genomic annotation. Refer to the paper for specific details of the overlap score.
---
+++ lean_go_shifter.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python
+
+
+import argparse
+import math
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+
+
+def parse_arguments():
+
+    '''
+    Parse and validate command line arguments.
+
+    :return: A namespace containing the command line argument values.
+    '''
+
+    parser = argparse.ArgumentParser(description='A lean reimplementation of GoShifter')
+
+    parser.add_argument('snp_map', help='A table of SNPs for each individual signal.')
+    parser.add_argument('annotation', help='File with genomic regions representing annotations.')
+    parser.add_argument('permute', help='Number of permutations for the null distribution.')
+    parser.add_argument('out_dir', help='Output directory.')
+    parser.add_argument('prefix', help='Prefix for output files.')
+
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.snp_map):
+        print(f'{args.snp_map} is not a valid path to a file', file=sys.stderr)
+        exit(2)
+    
+    if not os.path.isfile(args.annotation):
+        print(f'{args.annotation} is not a valid path to a file', file=sys.stderr)
+        exit(2)
+    
+    try:
+        args.permute = int(args.permute)
+    except ValueError:
+        print(f'{args.permute} is not an integer', file=sys.stderr)
+        exit(2)
+    
+    if args.permute <= 0:
+        print('Permutations must be greater than 0', file=sys.stderr)
+        exit(2)
+
+    return args
+
+
+def read_snp_map(file_path):
+
+    '''
+    Reads the list of fine-mapped SNP sets from the flat file provided.
+
+    :param file_path: File path to the SNP map.
+    :return: A data frame of the snp map.
+    '''
+
+    snp_map = pd.read_csv(file_path, sep='\t', dtype={'Chr': str, 'Position': int})
+    snp_map.sort_values(['Chr', 'Position'], ignore_index=True, inplace=True)
+
+    return snp_map
+
+
+def read_annotations(file_path):
+
+    '''
+    Reads the list of annotations from the flat file provided.
+
+    :param file_path: File path to the annotations.
+    :return: A data frame of the annotations.
+    '''
+
+    annotations = pd.read_csv(file_path, sep='\t', dtype={'Chr': str, 'Start': int, 'End': int})
+    annotations.sort_values(['Chr', 'Start', 'End'], ignore_index=True, inplace=True)
+
+    return annotations
+
+
+def infer_loci(snp_map, annotations):
+
+    '''
+    Infers the genomic range for each locus. Takes the minimum and maximum positions provided
+    per set of SNPs within a locus and adds the median width of an annotation to the ends.
+
+    :param snp_map: Set of SNPs at each locus.
+    :param annotations: Set of annotations over genome.
+    :return: A data frame of ranges for the loci.
+    '''
+
+    median_annotation_width = math.floor((annotations['End'] - annotations['Start']).median())
+
+    loci = snp_map.groupby('Locus').agg({
+        'Chr': 'first',
+        'Position': [
+            lambda x: x.min() - 2 * median_annotation_width,
+            lambda x: x.max() + 2 * median_annotation_width
+        ]
+    })
+    loci.columns = loci.columns.droplevel(0)
+    loci.columns = ['Chr', 'Start', 'End']
+    loci.reset_index(inplace=True)
+    loci.sort_values(['Chr', 'Start', 'End'], ignore_index=True, inplace=True)
+
+    return loci
+
+
+def snps_overlap_with_annotations(snp_map, annotations):
+
+    '''
+    Tests if any SNP in the list overlaps any annotation.
+
+    :param snp_map: The list of SNPs to test for overlap.
+    :param annotations: The annotations to test for overlap.
+    :return: True if any SNP overlaps any annotation.
+    '''
+
+    annotation_index = 0
+    snp_index = 0
+
+    annotation_iterator = annotations.iterrows()
+    snp_iterator = snp_map.iterrows()
+
+    annotation_index, annotation = next(annotation_iterator, (None, None))
+    snp_index, snp = next(snp_iterator, (None, None))
+
+    # Iterate until either the annotations or SNPs are all considered for overlap
+    while annotation_index is not None and snp_index is not None:
+
+        # If the annotation occurs before the SNP by genomic coordinates, increment the annotation
+        # If the SNP occurs before the annotation by genomic coordinates, increment the SNP
+        if annotation.Chr == snp.Chr:
+            if annotation.Start <= snp.Position <= annotation.End:
+                return True
+                snp_index, snp = next(snp_iterator, (None, None))
+            elif snp.Position < annotation.Start:
+                snp_index, snp = next(snp_iterator, (None, None))
+            else:
+                annotation_index, annotation = next(annotation_iterator, (None, None))
+        elif annotation.Chr < snp.Chr:
+            annotation_index, annotation = next(annotation_iterator, (None, None))
+        else:
+            snp_index, snp = next(snp_iterator, (None, None))
+
+    return False
+
+
+def permute_locus(locus, snp_map, annotations, iters):
+
+    '''
+    Calculates the observed overlap with annotations and then performs permutations to
+    compute a null distribution for the locus.
+
+    :param locus: The bounds of the locus.
+    :param snp_map: The list of SNPs at the locus.
+    :param annotations: The annotations at the locus to permute.
+    :param iters: The number of permutations to build the null distribution.
+    :return: A tuple containing the observed overlap and the null distribution.
+    '''
+
+    if len(annotations) == 0:
+        return 0, np.zeros((iters,))
+
+    # Restrict annotations that are outside the locus bounds to strictly within the locus region
+    annotations['Start'] = annotations['Start'].apply(lambda x: np.max((locus.Start, x)))
+    annotations['End'] = annotations['End'].apply(lambda x: np.min((locus.End, x)))
+
+    # Determine the observed overlap at the locus
+    observed = int(snps_overlap_with_annotations(snp_map, annotations))
+
+    # Build null distribution
+    locus_width = locus.End - locus.Start + 1
+    iterations = list()
+
+    for i in range(iters):
+
+        # Shift a random amount within the locus
+        shift = np.random.randint(0, locus_width)
+
+        # Add shift value to all genomic coordinates of the annotation
+        annotations_iter = annotations.copy()
+        annotations_iter[['Start', 'End']] = annotations_iter[['Start', 'End']].add(shift)
+
+        # Identify any annotations that overflow (go past the boundary of the locus)
+        annotations_iter_overflow = annotations_iter.copy()
+        annotations_iter_overflow = annotations_iter_overflow.loc[annotations_iter_overflow['End'] > locus.End, :]
+
+        # "Circularize" by mapping overflowing elements to the beginning of the locus
+        annotations_iter_overflow['Start'] = locus.Start + (annotations_iter_overflow['Start'] - locus.End - 1)
+        annotations_iter_overflow['End'] = locus.Start + (annotations_iter_overflow['End'] - locus.End - 1)
+
+        # Trim overflowing elements to be within the locus
+        annotations_iter['End'] = annotations_iter['End'].apply(lambda x: np.min((locus.End, x)))
+        
+        # Add back overflow segments and sort annotations for overlap test
+        annotations_iter = pd.concat((annotations_iter_overflow, annotations_iter))
+        annotations_iter.sort_values(['Chr', 'Start', 'End'], ignore_index=True, inplace=True)
+
+        # Store iteration result
+        iter_result = int(snps_overlap_with_annotations(snp_map, annotations_iter))
+        iterations.append(iter_result)
+
+    return observed, np.array(iterations)
+
+
+def main():
+
+    # Read command line arguments
+    args = parse_arguments()
+
+    # Read SNP map and annotation
+    snp_map = read_snp_map(args.snp_map)
+
+    annotations = read_annotations(args.annotation)
+
+    # Infer loci from SNP map and annotations
+    loci = infer_loci(snp_map, annotations)
+
+    # Process each locus
+    observed_dist = np.zeros((len(loci),))
+    null_dist = np.zeros((args.permute, len(loci)))
+
+    for locus_index, locus in loci.iterrows():
+
+        # Identify SNPs and annotations at the locus
+        snps_at_locus = snp_map.copy().loc[locus.Locus == snp_map['Locus'], :]
+        snps_at_locus.reset_index(drop=True, inplace=True)
+
+        annotations_at_locus = annotations.copy().loc[(locus.Chr == annotations['Chr']) & (locus.Start <= annotations['End']) & (annotations['Start'] <= locus.End), :]
+        annotations_at_locus.reset_index(drop=True, inplace=True)
+        
+        # Perform permutations
+        observed, iterations = permute_locus(locus, snps_at_locus, annotations_at_locus, args.permute)
+
+        # Update observed and null distributions
+        observed_dist[locus_index] = observed
+        null_dist[:, locus_index] = iterations
+    
+    # Calculate statistics over count distribution
+    count_observed = observed_dist.sum()
+
+    count_null_dist = null_dist.sum(axis=1)
+
+    p_value = (count_null_dist >= count_observed).sum() / args.permute
+
+    with open(os.path.join(args.out_dir, f'{args.prefix}_p_value.tsv'), 'w') as f_out:
+        f_out.write(f'pvalue\t{p_value}')
+
+    # Calculate overlap scores for each locus
+    overlap_scores = null_dist.sum(axis=0) / args.permute
+
+    loci['Overlap'] = observed_dist
+    loci['Overlap_Score'] = overlap_scores
+    loci.sort_values(['Overlap', 'Overlap_Score'], ascending=[False, True], inplace=True)
+
+    loci.to_csv(os.path.join(args.out_dir, f'{args.prefix}_overlap_scores.tsv'), sep='\t', index=False)
+
+
+if __name__ == '__main__':
+
+    main()
